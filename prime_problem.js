808800406e037fe4f21c0af6ff9345450f353667
jayesh1897 <jayeshsoni1211997@gmail.com>
2022-09-13 19:02:30 +0530

    prime_problem file created

---
+++ prime_problem.js
@@ -0,0 +1,15 @@
+//printing the prime no. from 1 to 20
+
+let num = 20;
+let count=0;
+
+for(i=1;i<=20;i++){
+    for(j=1;j<=20;j++){
+        if(i%j==0){
+           count++;
+        }
+    }
+} 
+if(count==2){
+    
+}
