3d28bff83b8c3d730fd8ec49481c4e93cc6684c8
jayesh1897 <jayeshsoni1211997@gmail.com>
2022-09-19 22:28:00 +0530

    file created for problem-2

---
+++ palindrome_problem.js
@@ -0,0 +1,16 @@
+  let x = "naman";
+ let bag1="";
+ let bag2="";
+ 
+ for(let i=0; i<=x.length-1; i++){
+     bag1 = bag1 + x[i];
+ }
+ 
+ for(let j=x.length-1; j>=0; j--){
+     bag2 = bag2 + x[j];
+ }
+  if(bag1==bag2){
+      console.log("Yes");
+  }else{
+      console.log("No");
+  }
